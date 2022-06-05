f89357c5d4265ee2642a820a42f584d2d05b8a60
Mirzayevsky <mirzayevskydev@gmail.com>
2022-06-05 21:16:11 +0500

    changed App.js file's function

--- src/App.js
+++ src/App.js
@@ -27,15 +27,19 @@ function App() {
     const [select, setSelect] = useState("")
     const [search,setSearch] = useState("")
 
-    const SortedPosts = useMemo ( () => {
+    const sortedPosts =  useMemo(()=>{
+        console.log('render');
         if (select) {
             return [...posts].sort((a, b) => a[select].localeCompare(b[select]))
         }
         return posts
- } ,[select,posts])
-    const sortedPosts  = SortedPosts()
+    },[select,posts])
 
-
+    const sortedAndSearchPosts  = useMemo (()=>{
+return sortedPosts.filter(post => post.title.toLowerCase().trim().includes(search.toLowerCase().trim()))
+    },[search,sortedPosts])
+        
+    
 
     //call back function
     const createPost = (newPost) => {
@@ -45,11 +49,9 @@ function App() {
         setPosts(posts.filter(s => s.id !== post.id))
     }
   
-
     const sortPost = (sort) => {
         setSelect(sort)
     }
-   
 
     return (
         <div className={'app'}>
@@ -57,7 +59,12 @@ function App() {
                 <div className={'main-title'}>Create Your First Post</div>
                 <PostData createPost={createPost}/>
                 <div className={'select-wrap'}>
-                    <Input className={'search-input'} placeholder={'Search...'}/>
+                    <Input 
+                     className={'search-input'}
+                     placeholder={'Search...'}
+                     search={search}
+                     onChange={(e) => setSearch(e.target.value)}
+                     />
                     <Select
                         value={select}
                         onChange={sortPost}
@@ -69,7 +76,7 @@ function App() {
                     />
                 </div>
                 <div className={'main-title'}>Programming Language</div>
-                {posts.length ? <Table remove={deletePost} posts={SortedPosts}/> :
+                {sortedAndSearchPosts.length ? <Table remove={deletePost} posts={sortedAndSearchPosts}/> :
                     <h3 className={'error-title'}>User did not posted yet !</h3>}
             </div>
         </div>
